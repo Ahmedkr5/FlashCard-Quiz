3770c0aee60b22054f79b0af6bf3439a202ee2cf
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-10-07 14:09:24 +0100

    Creating sample flashcard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import Flashcardlist from './components/FlashcardList'
+
 
 function App() {
+  const[flashcards ,setFlashcards] =useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flashcardlist flashcards={flashcards}></Flashcardlist>
   );
 }
 
+
+
+const SAMPLE_FLASHCARDS =[
+  {
+    id: 1,
+    question: 'What is 2+2?',
+    answer: '4',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'question 2?',
+    answer: 'answer',
+    options:[
+      'answer 1',
+      'answer 2',
+      'answer 3',
+      'answer 4'
+    ]
+  }
+ 
+]
+
+
+
+
+
+
+
+
+
 export default App;
+
